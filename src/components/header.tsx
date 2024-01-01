ae2e4329bd30437f9a38af65296a119bc2696594
innovin <innovinanuonye@gmail.com>
2024-01-01 17:45:16 +0100

    remove console

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom";
 const Header = () => {
     return (
         <header>
-            <nav className="!flex flex-row w-full">
+            <nav className="!flex flex-row w-full justify-between">
                 <p className="w-[40%]">Innovin Dev's Portfolio</p>
                 <ul className="w-[40%] hidden md:flex gap-2">
                     <li><Link to={'/about'}>About Me</Link></li>
